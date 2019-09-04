4cb64bd43a10f64d20d2a6757f66271909cf5f5d
hestonhoffman <hestonhoffman@gmail.com>
2019-09-04 12:34:38 -0700

    Adding variables

--- docs/readmes/git_to_dockerhub.rb
+++ docs/readmes/git_to_dockerhub.rb
@@ -7,6 +7,15 @@ end
 
 readmes = list_readmes("available_tasks.yaml")
 
-readmes.each do |name, prefix, repo |
-    puts "#{name}, #{prefix}, #{repo}"
-end
+puts readmes
+
+# Vreadmes.each do |name, prefix, repo, md_filename |
+#    puts "#{name}, #{prefix}, #{repo}, #{md_filename}"
+#    `docker run --rm \
+#    -v /$HOME/repos/nebula-docs/docs/readmes/#{:md_filename}:/data/README.md \
+#    -e DOCKERHUB_USERNAME=${DOCKERHUB_USERNAME} \
+#    -e DOCKERHUB_PASSWORD=${DOCKERHUB_PASSWORD} \
+#    -e DOCKERHUB_REPO_PREFIX=#{:prefix} \
+#    -e DOCKERHUB_REPO_NAME=#{:repo} \
+#     readme-to-hub`
+#end
