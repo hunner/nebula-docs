a8d489c29664331afb93c0d3483e4f5f0e642efc
hestonhoffman <hestonhoffman@gmail.com>
2019-09-05 22:30:01 -0700

    updating ruby script

--- readmes/git_to_dockerhub.rb
+++ readmes/git_to_dockerhub.rb
@@ -1,23 +1,19 @@
 require 'yaml'
 
 def list_readmes(filename)
-     data = YAML.load(File.read(filename))
-     # data["readmes"]
+    data = YAML.load(File.read(filename))
 end
 
 filename = "available_tasks.yaml"
 
 readmes = list_readmes(filename)
-# puts readmes
 
-readmes.each do |one, two|
-    puts "#{one} #{two}"
-#    puts "#{readmes[:name]}, #{readmes[:prefix]}, #{readmes[:repo]}, #{readmes[:md_filename]}"
-#    `docker run --rm \
-#    -v /$HOME/repos/nebula-docs/docs/readmes/#{:md_filename}:/data/README.md \
-#    -e DOCKERHUB_USERNAME=${DOCKERHUB_USERNAME} \
-#    -e DOCKERHUB_PASSWORD=${DOCKERHUB_PASSWORD} \
-#    -e DOCKERHUB_REPO_PREFIX=#{:prefix} \
-#    -e DOCKERHUB_REPO_NAME=#{:repo} \
-#     readme-to-hub`
+readmes.each do |value|
+    `docker run --rm \
+    -v /$HOME/repos/nebula-docs/docs/readmes/#{value["md_filename"]}:/data/README.md \
+    -e DOCKERHUB_USERNAME=${DOCKERHUB_USERNAME} \
+    -e DOCKERHUB_PASSWORD=${DOCKERHUB_PASSWORD} \
+    -e DOCKERHUB_REPO_PREFIX=#{value["dockerhub_repo_prefix"]} \
+    -e DOCKERHUB_REPO_NAME=#{value["dockerhub_repo_name"]} \
+     readme-to-hub`
 end
